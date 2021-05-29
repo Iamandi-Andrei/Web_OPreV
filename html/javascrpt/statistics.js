f14593b3e2dcc6204233e92023c10aeb4bdeee33
vasiledanutmirila <79104941+vasiledanutmirila@users.noreply.github.com>
2021-05-29 15:44:30 +0300

    Added statistics.js

---
+++ html/javascrpt/statistics.js
@@ -0,0 +1,15 @@
+function getVisits(value) {
+    document.getElementById("results").innerHTML = "There are " + value + " visits to the site.";
+}
+
+function getTable(value) {
+    document.getElementById("results").innerHTML = "There are " + value + " searchs for table.";
+}
+
+function getLine(value) {
+    document.getElementById("results").innerHTML = "There are " + value + " searchs for line.";
+}
+
+function getBar(value) {
+    document.getElementById("results").innerHTML = "There are " + value + " searchs for bar.";
+}
