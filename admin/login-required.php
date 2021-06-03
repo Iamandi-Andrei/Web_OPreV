64ddfd2443f2cf3211f393536d58c947668893c5
vasiledanutmirila <79104941+vasiledanutmirila@users.noreply.github.com>
2021-06-03 10:36:41 +0300

    Modified login-required

--- admin/login-required.php
+++ admin/login-required.php
@@ -1,27 +1,20 @@
 <?php
-    session_start();
 
-    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
-    $url = "https://";   
-    else  
-        $url = "http://";   
-    // Append the host(domain name, ip) to the URL.   
-    $url.= $_SERVER['HTTP_HOST'];   
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
+$url = "https://";   
+else  
+	$url = "http://";   
+// Append the host(domain name, ip) to the URL.   
+$url.= $_SERVER['HTTP_HOST'];   
 
-    // Append the requested resource location to the URL   
-    $url.= $_SERVER['REQUEST_URI'];
-    $pos=strpos($url,"admin/login-required.php");
-    $url=substr($url,0,$pos);
+// Append the requested resource location to the URL   
+$url.= $_SERVER['REQUEST_URI'];
+$pos=strpos($url,"admin/login-required.php");
+$url=substr($url,0,$pos);
 
-    $dom = new DomDocument; // instanțiem un obiect DOM
-    libxml_use_internal_errors(true);
-    $dom->loadHTMLFile($url."html/admin.php");
-    libxml_use_internal_errors(false);
-    
-    $location=$dom->getElementById("display-area");
-    $nodeP=$dom->createElement("p");
-    $nodeP->nodeValue='You need to login!';
-    $location->appendChild($nodeP);
-
-    echo $dom->saveHTML();
 ?>
+
+<script>
+	window.location = '<?php echo "http://localhost/Web_OPreV/" . "html/admin.php"; ?>';
+	alert('<?php echo "You need to login!"; ?>')
+</script>
