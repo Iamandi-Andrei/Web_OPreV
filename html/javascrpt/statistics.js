646835627ac21fb64f4420005b2f95854249dcc5
vasiledanutmirila <79104941+vasiledanutmirila@users.noreply.github.com>
2021-06-05 18:13:19 +0300

    Added AJAX for statistics

--- html/javascrpt/statistics.js
+++ html/javascrpt/statistics.js
@@ -1,15 +1,45 @@
-function getVisits(value) {
-    document.getElementById("results").innerHTML = "There are " + value + " visits to the site.";
+function getVisits() {
+	var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("results").innerHTML = "There are "+this.responseText+" visits to the site.";
+    }
+  };
+  xmlhttp.open("GET", "./../admin/visits-check.php?visit_type=visits", true);
+  xmlhttp.send();
+    
 }
 
-function getTable(value) {
-    document.getElementById("results").innerHTML = "There are " + value + " searchs for table.";
+function getTable() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("results").innerHTML = "There are "+this.responseText+" searches for table.";
+    }
+  };
+  xmlhttp.open("GET", "./../admin/visits-check.php?visit_type=table", true);
+  xmlhttp.send();
 }
 
-function getLine(value) {
-    document.getElementById("results").innerHTML = "There are " + value + " searchs for line.";
+function getLine() {
+	var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("results").innerHTML = "There are "+this.responseText+" searches for line.";
+    }
+  };
+  xmlhttp.open("GET", "./../admin/visits-check.php?visit_type=line", true);
+  xmlhttp.send();
+    
 }
 
 function getBar(value) {
-    document.getElementById("results").innerHTML = "There are " + value + " searchs for bar.";
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("results").innerHTML = "There are "+this.responseText+" searches for bar.";
+    }
+  };
+  xmlhttp.open("GET", "./../admin/visits-check.php?visit_type=bar", true);
+  xmlhttp.send();
 }
