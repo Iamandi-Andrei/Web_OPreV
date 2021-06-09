7517c06b929ddd5871ad71cfef5382d1d9c569de
vasiledanutmirila <79104941+vasiledanutmirila@users.noreply.github.com>
2021-06-09 12:52:02 +0300

    database configuration + missing fields fix

--- api/database/CRUD/delete.php
+++ api/database/CRUD/delete.php
@@ -12,7 +12,8 @@ $database = new Database();
 $db = $database->getConnection();
   
 $results=array();
-if(isset($_POST['country'])&&isset($_POST['age'])&&isset($_POST['year'])&&isset($_POST['gender'])&&isset($_POST['BMI_type'])){
+if(isset($_POST['country'])&&isset($_POST['age'])&&isset($_POST['year'])&&isset($_POST['gender'])&&isset($_POST['BMI_type']) &&
+	$_POST['country'] != "" && $_POST['age'] != "" && $_POST['year'] != "" && $_POST['gender'] != "" && $_POST['BMI_type'] != ""){
 	$statement=$db->prepare("delete from data where BMI_type= ? and year = ? and country = ? and age = ? and gender = ?");
 	$statement->bind_param("sisss",$_POST['BMI_type'],$_POST['year'],$_POST['country'],$_POST['age'],$_POST['gender']);
 	
--- api/database/CRUD/update-create.php
+++ api/database/CRUD/update-create.php
@@ -12,44 +12,43 @@ $database = new Database();
 $db = $database->getConnection();
   
 $results=array();
-if(isset($_POST['country'])&&isset($_POST['age'])&&isset($_POST['year'])&&isset($_POST['gender'])&&isset($_POST['BMI_type'])&&isset($_POST['BMI_value'])){
+if(isset($_POST['country'])&&isset($_POST['age'])&&isset($_POST['year'])&&isset($_POST['gender'])&&isset($_POST['BMI_type'])&&isset($_POST['BMI_value'])&&
+	$_POST['country'] != "" && $_POST['age'] != "" && $_POST['year'] != "" && $_POST['gender'] != "" && $_POST['BMI_type'] != "" && $_POST['BMI_value'] != ""){
 	$statement=$db->prepare("select * from data where BMI_type= ? and year = ? and country = ? and age = ? and gender = ?");
 	$statement->bind_param("sisss",$_POST['BMI_type'],$_POST['year'],$_POST['country'],$_POST['age'],$_POST['gender']);
 	
+	$statement->execute();
+	if (!($rez = $statement->get_result())) {
+		die ('A survenit o eroare la interogare');
+	}
+
+
+	if ($inreg = $rez->fetch_assoc()) {
+		array_push($results,$inreg);
+		echo "Data already existent. Updating values";
+		
+		$statement=$db->prepare("delete from data where BMI_type= ? and year = ? and country = ? and age = ? and gender = ?");
+		$statement->bind_param("sisss",$_POST['BMI_type'],$_POST['year'],$_POST['country'],$_POST['age'],$_POST['gender']);
+		$statement->execute();
+		
+		$statement=$db->prepare("insert into data (BMI_type, year ,country , age , gender,BMI_value) values(?,?,?,?,?,?)");
+		$statement->bind_param("sisssd",$_POST['BMI_type'],$_POST['year'],$_POST['country'],$_POST['age'],$_POST['gender'],$_POST['BMI_value']);
+		$statement->execute();
+		
+		
+	}
+	else{
+		$output = "Row inserted";
+		$statement=$db->prepare("insert into data (BMI_type, year ,country , age , gender,BMI_value) values(?,?,?,?,?,?)");
+		$statement->bind_param("sisssd",$_POST['BMI_type'],$_POST['year'],$_POST['country'],$_POST['age'],$_POST['gender'],$_POST['BMI_value']);
+		$statement->execute();
+		
+	}
 }
 else $output = "Missing fields";
 
 //echo $sql;
-$statement->execute();
 
-
-
-                if (!($rez = $statement->get_result())) {
-                    die ('A survenit o eroare la interogare');
-                }
-				
-
-                if ($inreg = $rez->fetch_assoc()) {
-					array_push($results,$inreg);
-					echo "Data already existent. Updating values";
-					
-					$statement=$db->prepare("delete from data where BMI_type= ? and year = ? and country = ? and age = ? and gender = ?");
-					$statement->bind_param("sisss",$_POST['BMI_type'],$_POST['year'],$_POST['country'],$_POST['age'],$_POST['gender']);
-					$statement->execute();
-					
-					$statement=$db->prepare("insert into data (BMI_type, year ,country , age , gender,BMI_value) values(?,?,?,?,?,?)");
-					$statement->bind_param("sisssd",$_POST['BMI_type'],$_POST['year'],$_POST['country'],$_POST['age'],$_POST['gender'],$_POST['BMI_value']);
-					$statement->execute();
-					
-					
-                }
-				else{
-					$output = "Row inserted";
-					$statement=$db->prepare("insert into data (BMI_type, year ,country , age , gender,BMI_value) values(?,?,?,?,?,?)");
-					$statement->bind_param("sisssd",$_POST['BMI_type'],$_POST['year'],$_POST['country'],$_POST['age'],$_POST['gender'],$_POST['BMI_value']);
-					$statement->execute();
-					
-				}
 
 $db->close();
 
